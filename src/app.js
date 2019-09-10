00da23081be435914570b94c56a5b077eb9e1f13
Steffen <steffen.haaker@gmail.com>
2019-09-09 23:09:06 -0400

    Added sendMessage to send custom message

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ console.log('compiled')
 const IPFS = require('ipfs')
 const Room = require('ipfs-pubsub-room')
 
-// console.log(IPFS)
+console.log(IPFS)
 
 const ipfs = new IPFS({
     repo: repo(),
@@ -25,6 +25,8 @@ ipfs.once('ready', () => ipfs.id((err, info) => {
     console.log('Address: ' + info.id)
 }))
 
+console.log(ipfs)
+
 const room = Room(ipfs, 'ipfs-pubsub-rooms')
 
 console.log(room)
@@ -36,10 +38,40 @@ room.on('peer joined', (peer) => room.sendTo(peer, 'Hello ' + peer))
 room.on('message', (message) => 
     console.log('got message from ' + message.from + ' : ' + message.data.toString()))
 
+room.on('subscribed', () => {
+    console.log('connected')
+})
+
     // testing broadcast every 2 seconds
 // setInterval(() => room.broadcast('testing broadcast'), 2000)
+// room.hasPeer(peer)
+function sendMsgOnClick() {
+    let getMessage = document.getElementById("msgInput").value
+    console.log(getMessage)
+    // room.hasPeer(peer)
+
+    console.log(room.getPeers())
+    let peers = room.getPeers()
+    room.broadcast("THIS IS A MESSAGE ONCLICK")
+    room.broadcast("input message: " + getMessage)
+    room.sendTo(peers[0], "Message to first peer in list")
+    room.sendTo(peers[1], "Message to second peer in list")
+}
+let sendButton = document.getElementById('sendButton');
+sendButton.addEventListener('click', sendMsgOnClick);
+
+function testFunction () {
+    console.log('testing')
+}
+var button = document.getElementById('testButton'); 
+button.addEventListener('click', testFunction);
 
 function repo () {
     // returns random repo path to have different ID's
+    // random ID's generated each reload
     return 'ipfs/pubsub/' + Math.random()
 }
+
+testFunction()
+
+
