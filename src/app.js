6cb4a09d9805362e0f5ad99ed99ae9afda1d59e9
Steffen <steffen.haaker@gmail.com>
2019-10-07 20:08:33 -0400

    Minor fixes

--- src/app.js
+++ src/app.js
@@ -158,10 +158,13 @@ window.clickPeer = function(peerID){
 };
 
 window.showPeer = function(){
-    let x = document.getElementById("sidemenu");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+    if (x != null) {
+        if (x.style.display == "block") {
+            x.style.display = 'none';
+        }
+        else {
+            x.style.display = 'block';
+        }
+        return false;
     }
 };
